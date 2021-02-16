d30e0d45d554cea2d28e60ad94df5685d85e1e20
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2021-02-16 16:33:12 -0300

    Lendo Stream de Dados do Firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,17 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({Key key}) : super(key: key);
+  // const ChatScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, i) => Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Funcionou!'),
-              )),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final documents = snapshot.data.docs;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, i) => Container(
+              padding: EdgeInsets.all(10),
+              child: Text(documents[i]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
