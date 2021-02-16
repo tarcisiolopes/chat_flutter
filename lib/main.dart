369ce7c86913a73acdb4b21b7fc678f46b2b548f
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2021-02-16 17:53:15 -0300

    Tela de autenticação

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat/screens/auth_screen.dart';
 import 'package:chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
