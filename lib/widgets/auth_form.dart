ab55fe4c431a5f23f543c3ef9da3b2a0a50d58c8
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2021-02-19 16:47:03 -0300

    Evoluindo Tela Autenticação

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:chat/models/auth_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -6,6 +7,18 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final AuthData _authData = AuthData();
+
+  _submit() {
+    bool isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      print(_authData);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,32 +28,67 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Nome',
+                  if (_authData.isSignup)
+                    TextFormField(
+                      key: ValueKey('name'),
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                      ),
+                      initialValue: _authData.name,
+                      onChanged: (value) => _authData.name = value,
+                      validator: (value) {
+                        if (value == null || value.trim().length < 3) {
+                          return 'Nome deve ter no mínimo 3 caracteres.';
+                        }
+                        return null;
+                      },
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('email'),
                     decoration: InputDecoration(
                       labelText: 'E-mail',
                     ),
+                    onChanged: (value) => _authData.email = value,
+                    validator: (value) {
+                      if (value == null || !value.contains('@')) {
+                        return 'Forneça um e-mail válido.';
+                      }
+                      return null;
+                    },
                   ),
                   TextFormField(
+                    key: ValueKey('password'),
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Senha',
                     ),
+                    onChanged: (value) => _authData.password = value,
+                    validator: (value) {
+                      if (value == null || value.trim().length < 7) {
+                        return 'Nome deve ter no mínimo 7 caracteres.';
+                      }
+                      return null;
+                    },
                   ),
                   SizedBox(
                     height: 12,
                   ),
-                  RaisedButton(child: Text('Entrar'), onPressed: () {}),
+                  RaisedButton(
+                      child: Text(_authData.isLogin ? 'Entrar' : 'Cadastrar'),
+                      onPressed: _submit),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
-                    child: Text('Criar uma nova conta?'),
-                    onPressed: () {},
+                    child: Text(_authData.isLogin
+                        ? 'Criar uma nova conta?'
+                        : 'Já possui uma conta?'),
+                    onPressed: () {
+                      setState(() {
+                        _authData.toggleMode();
+                      });
+                    },
                   ),
                 ],
               ),
