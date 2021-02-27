14c1d3a84f538c75b39614281ba75ce9b3e0bc47
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2021-02-27 11:47:02 -0300

    Integrando Autenticação com Firebase #02

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat/models/auth_data.dart';
 import 'package:chat/widgets/auth_form.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,14 +14,32 @@ class _AuthScreenState extends State<AuthScreen> {
   final _auth = FirebaseAuth.instance;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  bool _isLoading = false;
+
   Future<void> _handleSubmit(AuthData authData) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    UserCredential authResult;
+
     try {
       if (authData.isLogin) {
-        await _auth.signInWithEmailAndPassword(
+        authResult = await _auth.signInWithEmailAndPassword(
             email: authData.email.trim(), password: authData.password);
       } else {
-        await _auth.createUserWithEmailAndPassword(
+        authResult = await _auth.createUserWithEmailAndPassword(
             email: authData.email, password: authData.password);
+
+        final userData = {
+          'name': authData.name,
+          'email': authData.email,
+        };
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(authResult.user.uid)
+            .set(userData);
       }
     } on PlatformException catch (err) {
       final msg = err.message ?? 'Ocorreu um erro! Verifique suas credenciais';
@@ -31,6 +50,10 @@ class _AuthScreenState extends State<AuthScreen> {
       );
     } catch (err) {
       print(err);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -39,7 +62,31 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(_handleSubmit),
+      // body: AuthForm(_handleSubmit),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Stack(
+              children: [
+                AuthForm(_handleSubmit),
+                if (_isLoading)
+                  Positioned.fill(
+                    child: Container(
+                      margin: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.5),
+                      ),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
